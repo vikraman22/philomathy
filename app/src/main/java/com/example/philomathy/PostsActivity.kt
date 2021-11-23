c058da3243e62d14ccba2ade3d8545405dfeecb0
vikraman22 <vikraman2223@gmail.com>
2021-11-23 08:58:48 +0530

    Added PostsActivity and Retrived Data

--- app/src/main/java/com/example/philomathy/PostsActivity.kt
+++ app/src/main/java/com/example/philomathy/PostsActivity.kt
@@ -2,10 +2,35 @@ package com.example.philomathy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 
+
+private const val TAG = "PostsActivity"
 class PostsActivity : AppCompatActivity() {
+
+    private lateinit var firestoreDb : FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posts)
+
+        firestoreDb = FirebaseFirestore.getInstance()
+        val postReference = firestoreDb.collection("Uploads")
+        postReference.addSnapshotListener{ snapshot, exception ->
+            if(exception != null || snapshot ==null)
+            {
+                Log.e(TAG, "Exception when querying posts", exception)
+                return@addSnapshotListener
+            }
+
+            for(document in snapshot.documents)
+                Log.i(TAG, "Document ${document.id}: ${document.data}")
+        }
+
     }
 }
+
+
+
+
--- app/src/main/java/com/example/philomathy/UploadActivity.java
+++ app/src/main/java/com/example/philomathy/UploadActivity.java
@@ -1,22 +1,32 @@
 package com.example.philomathy;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
 import java.util.UUID;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UploadActivity extends AppCompatActivity {
@@ -25,11 +35,10 @@ public class UploadActivity extends AppCompatActivity {
     ImageView img_preview;
     StorageReference storageReference;
     Button postbutton;
-
-    Uri filePath;
-
-    private final int PICK_IMAGE_REQUEST = 22;
-
+    DatabaseReference databaseReference;
+    private Uri filePath;
+    ProgressDialog progressDialog;
+    int PICK_IMAGE_REQUEST = 100;
     FirebaseStorage storage = FirebaseStorage.getInstance();
 
     @Override
@@ -45,120 +54,68 @@ public class UploadActivity extends AppCompatActivity {
         img_preview = findViewById(R.id.uploaded_view);
 
         upload_button.setOnClickListener(
-                v -> SelectImage());
+                v -> selectImage());
 
         postbutton.setOnClickListener(v -> uploadImage());
 
 
     }
 
-    private void SelectImage() {
-
+    private void selectImage() {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(
-                Intent.createChooser(
-                        intent,
-                        "Select Image from here..."),
-                PICK_IMAGE_REQUEST);
+        startActivityForResult(intent,  100);
     }
-
-    private void uploadImage() {
-
-        if (filePath != null) {
-
-            // Code for showing progressDialog while uploading
-            ProgressDialog progressDialog
-                    = new ProgressDialog(this);
-            progressDialog.setTitle("Uploading...");
-            progressDialog.show();
-
-            // Defining the child of storageReference
-            StorageReference ref
-                    = storageReference
-                    .child(
-                            "images/"
-                                    + UUID.randomUUID().toString());
-
-            // adding listeners on upload
-            // or failure of image
-            // Progress Listener for loading
-// percentage on the dialog box
-            ref.putFile(filePath)
-                    .addOnSuccessListener(
-                            taskSnapshot -> {
-
-                                // Image uploaded successfully
-                                // Dismiss dialog
-                                progressDialog.dismiss();
-                                Toast
-                                        .makeText(UploadActivity.this,
-                                                "Image Uploaded!!",
-                                                Toast.LENGTH_SHORT)
-                                        .show();
-                            })
-
-                    .addOnFailureListener(e -> {
-
-                        // Error, Image not uploaded
-                        progressDialog.dismiss();
-                        Toast
-                                .makeText(UploadActivity.this,
-                                        "Failed " + e.getMessage(),
-                                        Toast.LENGTH_SHORT)
-                                .show();
-                    })
-                    .addOnProgressListener(
-                            taskSnapshot -> {
-                                double progress
-                                        = (100.0
-                                        * taskSnapshot.getBytesTransferred()
-                                        / taskSnapshot.getTotalByteCount());
-                                progressDialog.setMessage(
-                                        "Uploaded "
-                                                + (int) progress + "%");
-                            });
-        }
-    }
-
 
     @Override
-    protected void onActivityResult(int requestCode,
-                                    int resultCode,
-                                    Intent data) {
-
-        super.onActivityResult(requestCode,
-                resultCode,
-                data);
-
-        // checking request code and result code
-        // if request code is PICK_IMAGE_REQUEST and
-        // resultCode is RESULT_OK
-        // then set image in the image view
-        if (requestCode == PICK_IMAGE_REQUEST
-                && resultCode == RESULT_OK
-                && data != null
-                && data.getData() != null) {
-
-            // Get the Uri of data
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK &&
+                data != null && data.getData() != null)
+        {
             filePath = data.getData();
-            try {
-
-                // Setting image on image view using Bitmap
-                Bitmap bitmap = MediaStore
-                        .Images
-                        .Media
-                        .getBitmap(
-                                getContentResolver(),
-                                filePath);
+            try
+            {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
                 img_preview.setImageBitmap(bitmap);
-            } catch (IOException e) {
-                // Log the exception
+            } catch (IOException e)
+            {
                 e.printStackTrace();
             }
         }
     }
+
+    private void uploadImage() {
+        if(filePath != null){ final ProgressDialog progress = new ProgressDialog(this); progress.setTitle("Uploading...."); progress.show();
+
+            StorageReference ref= storageReference.child("images/"+ UUID.randomUUID().toString());
+            ref.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    progress.dismiss();
+                    Toast.makeText(UploadActivity.this, "Uploaded successfully", Toast.LENGTH_SHORT).show();
+//                    String imageUrl = taskSnapshot.getDownloadUrl().toString(); /* Fetch url image */
+//                    Picasso.with(getBaseContext()).load(imageUrl).into(imgFirebase);
+//                     use picasso to fetch url and display image */
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progress.dismiss();
+                    Toast.makeText(UploadActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progres_time = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                    progress.setMessage("Uploaded "+(int)progres_time+" %");
+                }
+            });
+        }
+    }
+
+
 
 
 }
